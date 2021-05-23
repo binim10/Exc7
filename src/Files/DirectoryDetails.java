e64123ebf208f6cf3425a663c7c73101fe586e25
binim10 <57068936+binim10@users.noreply.github.com>
2021-05-23 21:17:14 +0300

    beginning composite

--- src/Files/DirectoryDetails.java
+++ src/Files/DirectoryDetails.java
@@ -1,11 +1,14 @@
 package Files;
 
+import java.util.ArrayList;
+
 // TODO: Implement Composite (change this file).
 public class DirectoryDetails extends FileDetails {
+    ArrayList<FileDetails> files= new ArrayList<>();
     public DirectoryDetails(String path, String name){
         super(path,name);
     }
     public void addFile(FileDetails fileDetails){
-        // TODO: complete
+       files.add(fileDetails);
     }
 }
